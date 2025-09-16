7c7d4eb415e2155b391eafc0d8161fc827180696
kominho4576-commits <kominho4576@gmail.com>
2025-09-17 00:04:27 +0900

    Update socket.ts

--- holdem-shot-full/client/src/lib/socket.ts
+++ holdem-shot-full/client/src/lib/socket.ts
@@ -1,1 +1,13 @@
-// socket placeholder
+import { io, Socket } from 'socket.io-client'
+
+const URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:8080'
+
+// Singleton socket
+let socket: Socket | null = null
+
+export function getSocket() {
+  if (!socket) {
+    socket = io(URL, { autoConnect: true, transports: ['websocket'] })
+  }
+  return socket
+}
