90525e165218fb52b96dc21b4ad3236154231c52
kominho4576-commits <kominho4576@gmail.com>
2025-09-17 00:04:11 +0900

    Update main.tsx

--- holdem-shot-full/client/src/main.tsx
+++ holdem-shot-full/client/src/main.tsx
@@ -1,1 +1,6 @@
-// main placeholder
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
