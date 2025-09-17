2bf91ab32e2e3c61e0ce820bf43333b45bc74b5a
kominho4576-commits <kominho4576@gmail.com>
2025-09-17 11:15:43 +0900

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,1 +1,10 @@
-// to be filled next
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  }
+});
