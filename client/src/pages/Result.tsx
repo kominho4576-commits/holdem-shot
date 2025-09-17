558d3cd66cbc4d7840f00c32ceebdccef28e3850
kominho4576-commits <kominho4576@gmail.com>
2025-09-17 11:18:54 +0900

    Update Result.tsx

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,1 +1,8 @@
-// next
+export default function Result() {
+  return (
+    <div className="center-col">
+      <div className="h1">Result</div>
+      <div className="card center">Victory / Defeat screen (5s auto home)</div>
+    </div>
+  );
+}
