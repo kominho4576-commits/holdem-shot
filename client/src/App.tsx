a33a41ea64e3890424269267f57e6958b31a24e9
kominho4576-commits <kominho4576@gmail.com>
2025-09-17 11:16:20 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,1 +1,17 @@
-// next
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Match from "./pages/Match";
+import Game from "./pages/Game";
+import Result from "./pages/Result";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/match" element={<Match />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/result" element={<Result />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
