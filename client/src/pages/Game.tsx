831dfd2f8177d7c8bce51c81fafd89ad11555880
kominho4576-commits <kominho4576@gmail.com>
2025-09-17 11:18:34 +0900

    Update Game.tsx

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,1 +1,10 @@
-// next
+export default function Game() {
+  return (
+    <div className="center-col">
+      <div className="h1">Game (coming next)</div>
+      <div className="card center">
+        Implementing Hold’em / Roulette views next...
+      </div>
+    </div>
+  );
+}
